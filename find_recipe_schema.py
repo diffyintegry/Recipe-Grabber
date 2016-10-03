e199793c99c149da0127d90920a664b4b43f953c
Nick Comment <ncomment@Nick-Comment.local>
2016-10-02 22:03:15 -0400

    recipe finding done

---
+++ find_recipe_schema.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+from BeautifulSoup import BeautifulSoup
+import requests as req
+import json
+from pprint import pprint
+
+
+def find_recipe(soup, recipes=[]):
+    ''' look for recipes in the given html
+    '''
+    for item in soup.findAll('script', type='application/ld+json'):
+        try:
+            recipeData = json.loads(item.contents[0])
+            assert recipeData['@type'] == 'Recipe'
+            recipes.append(recipeData)
+        except:
+            pass
+    return recipes
+
+
+
+def get_all_urls():
+    ''' crawl for urls
+    '''
+    pass
+
+
+
+def main():
+    ''' Get a recipe and pprint it
+    '''
+    doc = raw_input('what website?')
+    soup = BeautifulSoup(req.get(doc).text)
+    pprint( find_recipe(soup))
+
+
+
+if __name__=='__main__':
+    main()
